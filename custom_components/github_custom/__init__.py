033a73f7ae9756c747658eb1804559be1d735e9a
Aaron Godfrey <me@aarongodfrey.dev>
2024-08-14 19:58:23 -0700

    Fix deprecation warning
    https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/github_custom/__init__.py
+++ custom_components/github_custom/__init__.py
@@ -25,9 +25,7 @@ async def async_setup_entry(
     hass.data[DOMAIN][entry.entry_id] = hass_data
 
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
     return True
 
 
